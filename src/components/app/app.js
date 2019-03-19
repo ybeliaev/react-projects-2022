089ddcd80eee995bdec522d64d0186a210bb8aa3
Yura <ybeliaev@gmail.com>
2019-03-19 19:05:33 +0200

    comm

---
+++ src/components/app/app.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import ItemStatusFilter from "../item-status-filter";
+import List from "../list";
+
+import "./app.css";
+
+export default class App extends Component {
+  maxId = 10;
+  state = {
+    someData: [
+      this.createItem("Drink Coffee", true, true),
+      this.createItem("Create React App"),
+      this.createItem("Study Bible", false, true),
+      this.createItem("Read book of Hemingway")
+    ],
+    filter: "all"
+  };
+  createItem(label, done = false, important = false) {
+    return {
+      label,
+      done,
+      important,
+      id: this.maxId++
+    };
+  }
+  makeFilterChange = filter => {
+    this.setState({ filter });
+  };
+  render() {
+    const { someData } = this.state;
+
+    return (
+      <div className="app">
+        <h1>Filter of elements</h1>
+        <div className="top-panel">
+          <ItemStatusFilter
+            currentFilter={filter}
+            onFilterChange={this.makeFilterChange}
+          />
+        </div>
+        <List someProps={someData} onDeleted={this.makeDelete} />
+      </div>
+    );
+  }
+}
---
+++ src/components/list/list.js
@@ -0,0 +1,20 @@
+import React, { useLayoutEffect } from "react";
+
+import ListItem from "../list-item";
+import "./list-item.css";
+
+const List = ({ someData }) => {
+  const elements = someData.map(item => {
+    const { id, ...itemProps } = item;
+    return (
+      <li key={id}>
+        <ListItem
+          {...itemProps}
+          onImportant={() => onToggleImportant(id)}
+          onDone={() => onToggleDone(id)}
+        />
+      </li>
+    );
+  });
+  return <ul className="todo-list">{elements}</ul>;
+};
